37ff68189298e943923367f4e37508e5e99a92f9
ltabis <ltabis.pro@gmail.com>
2023-06-23 14:05:11 +0200

    remove unused function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 use rhai::{Engine, EvalAltResult};
 
 fn main() -> Result<(), Box<EvalAltResult>> {
-    // Define external function
-    fn compute_something(x: i64) -> bool {
-        (x % 40) == 0
-    }
-
     // Create scripting engine
     let mut engine = Engine::new();
 
-    // Register external function as 'compute'
-    engine.register_fn("compute", compute_something);
     engine.register_fn(
         "run",
         |ncc: rhai::NativeCallContext,
